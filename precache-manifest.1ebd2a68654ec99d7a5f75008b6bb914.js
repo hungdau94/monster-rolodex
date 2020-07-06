a6bb2967156fbc8512c7e3c2a9ae63382d6a0a96
Hung Dau <darklord2065@gmail.com>
2020-07-06 10:48:44 +0700

    Updates

---
+++ precache-manifest.1ebd2a68654ec99d7a5f75008b6bb914.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66c7680abc1384b5ef3e37b6fa5878da",
+    "url": "/https//hungdau94.github.io/monster-rolodex/index.html"
+  },
+  {
+    "revision": "5e7342f61de0f25d82cf",
+    "url": "/https//hungdau94.github.io/monster-rolodex/static/css/main.1d5b402b.chunk.css"
+  },
+  {
+    "revision": "7b555debf535f32f782e",
+    "url": "/https//hungdau94.github.io/monster-rolodex/static/js/2.9fc7c5d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/https//hungdau94.github.io/monster-rolodex/static/js/2.9fc7c5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e7342f61de0f25d82cf",
+    "url": "/https//hungdau94.github.io/monster-rolodex/static/js/main.c8b509ef.chunk.js"
+  },
+  {
+    "revision": "5de1a11f94e65e2da738",
+    "url": "/https//hungdau94.github.io/monster-rolodex/static/js/runtime-main.20ecc265.js"
+  }
+]);
